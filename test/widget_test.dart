44837b29d07d1af98e1df36c98663266afe49189
christin <christin@wednesday.is>
2020-07-27 23:07:17 +0530

    Add widget tests for WeatherList Widget
    Signed-off-by: christin <christin@wednesday.is>

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,10 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_template/main.dart';
+import 'package:flutter_template/routes/weather_route.dart';
+
+import 'package:flutter_template/model/weather.dart';
+import 'package:flutter_template/model/consolidated_weather.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+ /*  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
@@ -26,5 +29,24 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  }); */
+  testWidgets('WeatherList should display city name & weather details ', (WidgetTester tester) async {
+    var weatherData = [
+      ConsolidatedWeather( 1, 'test', 'tst', 'NE', 20, 30, 27, 60, 1.008, 99, 1, 60 )];
+    var input = [Weather('Pune', weatherData)];
+    await tester.pumpWidget(MaterialApp(
+      title: 'Flutter Test',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test'),
+        ),
+        body: Center(
+          child: WeatherList(input),
+        ),
+      ),
+    ));
+
+    // the List should display the City name provided
+    expect(find.text('Pune'), findsOneWidget);
   });
 }
