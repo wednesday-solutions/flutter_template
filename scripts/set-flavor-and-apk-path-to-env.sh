780fd48dae72cd634be1801873daf8dd7c52b91d
Shounak Mulay <shounakmulay@gmail.com>
2022-02-14 12:56:07 +0530

    Update CD workflow

--- scripts/set-flavor-and-apk-path-to-env.sh
+++ scripts/set-flavor-and-apk-path-to-env.sh
@@ -1,8 +1,8 @@
-#!bin/sh
+#!/bin/sh
 
 CURRENT_BRANCH=`git branch --show-current`
 
-if [[ "$CURRENT_BRANCH" == 'master' ]]; then
+if [[ "$CURRENT_BRANCH" == 'main' ]]; then
     echo "FLAVOR_NAME=prod" >> "$GITHUB_ENV"
     echo "APK_PATH=build/app/outputs/flutter-apk/app-prod-release.apk" >> "$GITHUB_ENV"
 else
