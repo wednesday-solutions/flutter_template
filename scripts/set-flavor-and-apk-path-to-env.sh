43e97bf11d820d29155f9deef9dd02df5f31c917
Shounak Mulay <shounakmulay@gmail.com>
2022-02-14 17:26:43 +0530

    Use apk path in release

--- scripts/set-flavor-and-apk-path-to-env.sh
+++ scripts/set-flavor-and-apk-path-to-env.sh
@@ -5,8 +5,7 @@
 #echo ""
 #
 #if [ "$CURRENT_BRANCH" == "main" ]; then
-#    echo "FLAVOR_NAME=prod" >> "$GITHUB_ENV"
-#    echo "APK_PATH=build/app/outputs/flutter-apk/app-prod-release.apk" >> "$GITHUB_ENV"
+
 #else
 #    echo "FLAVOR_NAME=qa" >> "$GITHUB_ENV"
 #    echo "APK_PATH=build/app/outputs/flutter-apk/app-qa-release.apk" >> "$GITHUB_ENV"
@@ -17,11 +16,10 @@ CURRENT_BRANCH=$(git branch --show-current)
 
 if [ "main" == "$CURRENT_BRANCH" ]; then
   echo "Current Branch is $CURRENT_BRANCH , proceeding with prod build"
-  bash $GRADLE_PATH assembleProdRelease
+  echo "FLAVOR_NAME=prod" >> "$GITHUB_ENV"
+  echo "APK_PATH=build/app/outputs/flutter-apk/app-prod-release.apk" >> "$GITHUB_ENV"
 else
   echo "Current Branch is $CURRENT_BRANCH, Proceeding with qa build"
-  RELEASE_OUTPUT_FILE=app/build/outputs/apk/qa/release/
-  bash $GRADLE_PATH assembleQaRelease
+  echo "FLAVOR_NAME=qa" >> "$GITHUB_ENV"
+  echo "APK_PATH=build/app/outputs/flutter-apk/app-qa-release.apk" >> "$GITHUB_ENV"
 fi
-
-echo "RELEASE_OUTPUT_FILE=$RELEASE_OUTPUT_FILE">>$GITHUB_ENV
