2c782a0ecaba1098778032921f26b8712560a4db
Shounak Mulay <shounakmulay@gmail.com>
2022-02-14 15:38:16 +0530

    Add skip ci commit message

--- scripts/commit-version.sh
+++ scripts/commit-version.sh
@@ -4,7 +4,7 @@ set -e
 # Commit and tag this version.
 version=`grep 'version: ' pubspec.yaml | sed 's/version: //'`
 
-git commit -m "Bump version to $version" pubspec.yaml --no-verify
+git commit -m "[skip ci] Bump version to $version" pubspec.yaml --no-verify
 
 CURRENT_BRANCH=$(git branch --show-current)
 
--- scripts/set-flavor-and-apk-path-to-env.sh
+++ scripts/set-flavor-and-apk-path-to-env.sh
@@ -2,7 +2,8 @@
 
 CURRENT_BRANCH=`git branch --show-current`
 
-if [[ "$CURRENT_BRANCH" == 'main' ]]; then
+if [ "$CURRENT_BRANCH" == "main" ]
+then
     echo "FLAVOR_NAME=prod" >> "$GITHUB_ENV"
     echo "APK_PATH=build/app/outputs/flutter-apk/app-prod-release.apk" >> "$GITHUB_ENV"
 else
