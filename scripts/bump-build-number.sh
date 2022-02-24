a0ca29da258c9831de4a22323a7b98f6de54e0ac
gtxtreme <gaurav.thakkar@wednesday.is>
2022-02-24 19:22:15 +0530

    Add FLUTTER_BUILD_NUMBER to Info.plist

--- scripts/bump-build-number.sh
+++ scripts/bump-build-number.sh
@@ -2,4 +2,7 @@
 set -e
 
 # Find and increment the version number.
+echo "Current Version: $(awk '/version: /{print $2}' pubspec.yaml)"
+echo "Updating the versionNumber by 1"
 perl -i -pe 's/^(version:\s+\d+\.\d+\.\d+\+)(\d+)$/$1.($2+1)/e' pubspec.yaml
+echo "New version: $(awk '/version: /{print $2}' pubspec.yaml)"
