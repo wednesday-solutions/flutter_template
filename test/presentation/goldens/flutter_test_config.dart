dfef63fdd196e989117852c8af531abcf05f0317
Shounak Mulay <shounakmulay@gmail.com>
2022-03-17 12:57:23 +0530

    Fix lint errors

--- test/presentation/goldens/flutter_test_config.dart
+++ test/presentation/goldens/flutter_test_config.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:golden_toolkit/golden_toolkit.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   TestWidgetsFlutterBinding.ensureInitialized();
