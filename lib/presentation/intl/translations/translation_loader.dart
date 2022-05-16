8b123d8759335a75555c8c59960f3bc70ee76e56
Saurav Maharana <saurav@wednesday.is>
2022-05-16 15:29:37 +0530

    formatting changes

--- lib/presentation/intl/translations/translation_loader.dart
+++ lib/presentation/intl/translations/translation_loader.dart
@@ -6,23 +6,22 @@ import 'dart:ui';
 
 import 'package:easy_localization/easy_localization.dart' show AssetLoader;
 
-class CodegenLoader extends AssetLoader {
+class CodegenLoader extends AssetLoader{
   const CodegenLoader();
 
   @override
-  Future<Map<String, dynamic>> load(String fullPath, Locale locale) {
+  Future<Map<String, dynamic>> load(String fullPath, Locale locale ) {
     return Future.value(mapLocales[locale.toString()]);
   }
 
-  static const Map<String, dynamic> en_US = {
-    "homePageTitle": "Home",
-    "searchPageTitle": "Search",
-    "forecast": "Forecast",
-    "favCitiesAppearHere":
-        "Weather for your favorite cities will appear here. Search cities to add to favorites.",
-    "startTypingToSearch": "Start typing to search",
-    "searchResultsAppearHere": "Search results appear here...",
-    "noResultsFound": "No results found..."
-  };
-  static const Map<String, Map<String, dynamic>> mapLocales = {"en_US": en_US};
+  static const Map<String,dynamic> en_US = {
+  "homePageTitle": "Home",
+  "searchPageTitle": "Search",
+  "forecast": "Forecast",
+  "favCitiesAppearHere": "Weather for your favorite cities will appear here. Search cities to add to favorites.",
+  "startTypingToSearch": "Start typing to search",
+  "searchResultsAppearHere": "Search results appear here...",
+  "noResultsFound": "No results found..."
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"en_US": en_US};
 }
