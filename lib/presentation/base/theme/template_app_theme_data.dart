db7562d4e0df236368ef200c9927241057e5dc3b
Saurav Maharana <saurav@wednesday.is>
2022-05-16 15:19:10 +0530

    added M3 Theming to App

--- lib/presentation/base/theme/template_app_theme_data.dart
+++ lib/presentation/base/theme/template_app_theme_data.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 final lightTheme = ThemeData(
   brightness: Brightness.light,
@@ -12,6 +14,28 @@ final darkTheme = ThemeData(
   snackBarTheme: const SnackBarThemeData(),
   appBarTheme: const AppBarTheme(),
   cardTheme: const _AppCardTheme(),
+);
+
+final m3Light = ThemeData(
+  useMaterial3: true,
+  fontFamily: GoogleFonts.signika().fontFamily,
+  snackBarTheme: const SnackBarThemeData(),
+  cardTheme: const _AppCardTheme(),
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: HexColor('#6c8cf5'),
+    brightness: Brightness.light,
+  ),
+);
+
+final m3Dark = ThemeData(
+  useMaterial3: true,
+  fontFamily: GoogleFonts.signika().fontFamily,
+  snackBarTheme: const SnackBarThemeData(),
+  cardTheme: const _AppCardTheme(),
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: HexColor('#6c8cf5'),
+    brightness: Brightness.dark,
+  ),
 );
 
 class _AppCardTheme extends CardTheme {
