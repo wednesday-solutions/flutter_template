473e4e007a1d2c305788443317b13bd66da6a6b2
Saurav Maharana <saurav@wednesday.is>
2022-05-17 12:03:24 +0530

    removed seed & added SchemeTheming

--- lib/presentation/base/theme/template_app_theme_data.dart
+++ lib/presentation/base/theme/template_app_theme_data.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template/presentation/base/theme/color_palette.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hexcolor/hexcolor.dart';
 
 final lightTheme = ThemeData(
   brightness: Brightness.light,
@@ -21,8 +21,17 @@ final m3Light = ThemeData(
   fontFamily: GoogleFonts.signika().fontFamily,
   snackBarTheme: const SnackBarThemeData(),
   cardTheme: const _AppCardTheme(),
-  colorScheme: ColorScheme.fromSeed(
-    seedColor: HexColor('#6c8cf5'),
+  colorScheme: ColorScheme(
+    background: lightColorScheme.background,
+    onBackground: lightColorScheme.onBackground,
+    error: lightColorScheme.error,
+    onError: lightColorScheme.onError,
+    onPrimary: lightColorScheme.onPrimary,
+    onSecondary: lightColorScheme.onSecondary,
+    onSurface: lightColorScheme.onSurface,
+    primary: lightColorScheme.primary,
+    secondary: lightColorScheme.secondary,
+    surface: lightColorScheme.surface,
     brightness: Brightness.light,
   ),
 );
@@ -32,8 +41,17 @@ final m3Dark = ThemeData(
   fontFamily: GoogleFonts.signika().fontFamily,
   snackBarTheme: const SnackBarThemeData(),
   cardTheme: const _AppCardTheme(),
-  colorScheme: ColorScheme.fromSeed(
-    seedColor: HexColor('#6c8cf5'),
+  colorScheme: ColorScheme(
+    background: darkColorScheme.background,
+    onBackground: darkColorScheme.onBackground,
+    error: darkColorScheme.error,
+    onError: darkColorScheme.onError,
+    onPrimary: darkColorScheme.onPrimary,
+    onSecondary: darkColorScheme.onSecondary,
+    onSurface: darkColorScheme.onSurface,
+    primary: darkColorScheme.primary,
+    secondary: darkColorScheme.secondary,
+    surface: darkColorScheme.surface,
     brightness: Brightness.dark,
   ),
 );
