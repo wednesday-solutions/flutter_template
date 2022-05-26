b96894124266929a6262812dac3190f69936846b
Shounak Mulay <shounakmulay@gmail.com>
2022-05-26 20:14:24 +0530

    Ignore secrets file

--- lib/secrets/app_secrets.skeleton.dart
+++ lib/secrets/app_secrets.skeleton.dart
@@ -1,1 +1,19 @@
-// const String openWeatherApiKey = "Your API key";
+// class AppSecrets {
+//   final String openWeatherApiKey;
+//
+//   const AppSecrets({
+//     required this.openWeatherApiKey,
+//   });
+//
+//   static const appSecretsDev = AppSecrets(
+//     openWeatherApiKey: "Your API Key",
+//   );
+//
+//   static const appSecretsQA = AppSecrets(
+//     openWeatherApiKey: "Your API Key",
+//   );
+//
+//   static const appSecretsProd = AppSecrets(
+//     openWeatherApiKey: "Your API Key",
+//   );
+// }
