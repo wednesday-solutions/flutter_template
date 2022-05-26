33e14dbe47cc5f8589d465be280c2eb06c7e3a67
Shounak Mulay <shounakmulay@gmail.com>
2022-05-26 17:33:11 +0530

    Setup secrets file to store api keys

--- lib/flavors/flavor_values.dart
+++ lib/flavors/flavor_values.dart
@@ -1,11 +1,15 @@
+import 'package:flutter_template/secrets/app_secrets.dart';
+
 class FlavorValues {
   final String apiBaseUrl;
   final bool showLogs;
+  final AppSecrets secrets;
 
   final bool _logSqlStatements;
 
   const FlavorValues({
     required this.apiBaseUrl,
+    required this.secrets,
     logSqlStatements = false,
     this.showLogs = false,
   }) : _logSqlStatements = showLogs && logSqlStatements;
---
+++ lib/secrets/app_secrets.skeleton.dart
@@ -0,0 +1,1 @@
+// const String openWeatherApiKey = "Your API key";
