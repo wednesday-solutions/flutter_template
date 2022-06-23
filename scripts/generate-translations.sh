ec98e6381792cc931756d72cc88efb43bab92bc3
Shounak Mulay <shounakmulay@gmail.com>
2022-06-23 20:29:23 +0530

    Update workflows

--- scripts/generate-translations.sh
+++ scripts/generate-translations.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-flutter pub run easy_localization:generate -S assets/translations -O lib/presentation/intl/translations -o translation_loader.dart
+flutter pub run easy_localization:generate -S assets/translations -O presentation/lib/intl/translations -o translation_loader.dart
 
-flutter pub run easy_localization:generate -S assets/translations -O lib/presentation/intl/translations -o translation_keys.dart -f keys
+flutter pub run easy_localization:generate -S assets/translations -O presentation/lib/intl/translations -o translation_keys.dart -f keys
