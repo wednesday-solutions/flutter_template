805f897516c66aad75f7e97d1d8aecd1ab1f1622
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-07-07 10:44:23 +0530

    adding extension function to buildContext to implement translation.

--- lib/foundation/extensions/string_ext.dart
+++ lib/foundation/extensions/string_ext.dart
@@ -1,13 +1,29 @@
 import 'package:easy_localization/easy_localization.dart' as el;
+import 'package:flutter/cupertino.dart';
 
 extension StringExt on String {
   bool get isBlank => trim().isEmpty;
 
-  String get tr {
+  // String get tr {
+  //   try {
+  //     return el.tr(this);
+  //   } catch (e) {
+  //     return this;
+  //   }
+  // }
+}
+
+///
+/// Extension on context to make sure any string that uses [tr] gets translated.
+/// Not using the context won't translate the string if its [tr] is being used in non changing components eg. viewModel
+///
+
+extension ContextExt on BuildContext {
+  String tr(String key) {
     try {
-      return el.tr(this);
+      return el.tr(key);
     } catch (e) {
-      return this;
+      return key;
     }
   }
 }
