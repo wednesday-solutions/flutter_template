945787c72082a5cf783da99029b127eea405832c
Ali hafizji <Ali@wednesdays-MacBook-Pro.local>
2022-11-01 23:17:16 +0530

    Adding default cities for both web & mobile

--- lib/services/base/database/connection/web.dart
+++ lib/services/base/database/connection/web.dart
@@ -1,7 +1,11 @@
-
 import 'package:drift/web.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_template/services/base/database/app_database.dart';
 
 AppDatabase connect() {
-  return AppDatabase(WebDatabase('db'));
+  return AppDatabase(WebDatabase('db', 
+      initializer: () async {
+        return (await rootBundle.load('assets/pre_populated_cities.sqlite')).buffer.asUint8List();
+      }));
+  //typedef CreateWebDatabase = FutureOr<Uint8List> Function();
 }
